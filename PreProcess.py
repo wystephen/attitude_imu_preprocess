11775d5711602a8be2d0b6b0a1d6a97ba39f4e0c
steve <551619855@qq.com>
2017-10-15 08:39:06 +0800

    Initial commit

---
+++ PreProcess.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -*-
+# Created by steve @ 17-10-15 上午8:36
+'''
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+'''
+
+import numpy as np
+import scipy as sp
+
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+
+    file_name = '/home/steve/Data/AttitudeIMU/2.txt'
+
+    data = np.loadtxt(file_name)
+
+    data[:,:3] = data[:,:3] / 10.0
+
+    plt.figure()
+    plt.title('attitude')
+    for i in range(3):
+        plt.plot(data[:,i],'-.',label = str(i))
+    plt.legend()
+
+
+
+
+
+    plt.show()
